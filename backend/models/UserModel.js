1dc54c3cec4d4957161e7e55b014d2671fa900bc
Tobias Rojas <tobiasurielrojas2004@gmail.com>
2025-10-17 17:25:59 -0300

    Fixing register failed exceptions

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -3,14 +3,11 @@ import { db } from '../db/db.js'
 export class UserModel {
   static async register (username, password) {
     try {
-      const query = 'INSERT INTO User (username, password) VALUES (?, ?)'
-      await db.execute(query, [username, password])
+      await db.execute('INSERT INTO "User" (username, password) VALUES (?, ?)', [username, password])
+      const result = await db.execute('SELECT id FROM "User" WHERE username = ?', [username])
 
-      const response = await db.execute('SELECT id FROM User WHERE username = ?', [username])
-
-      const newUser = { id: response.rows[0].id, username }
-
-      return newUser
+      const id = result.rows?.[0]?.[0]
+      return { id, username }
     } catch (error) {
       throw new Error('Error registering user: ' + error.message)
     }
@@ -18,19 +15,14 @@ export class UserModel {
 
   static async getUserByUsername (username) {
     try {
-      const query = 'SELECT * FROM User WHERE username = ?'
-      const response = await db.execute(query, [username])
-      const user = response.rows[0]
+      const result = await db.execute('SELECT * FROM "User" WHERE username = ?', [username])
+      if (!result.rows?.length) return undefined
 
-      if (!user) {
-        return undefined
-      }
+      const columns = result.columns
+      const values = result.rows[0]
+      const user = Object.fromEntries(columns.map((c, i) => [c, values[i]]))
 
-      return {
-        id: user.id,
-        username: user.username,
-        password: user.password
-      }
+      return user
     } catch (error) {
       throw new Error(`Error searching for user: ${username}`)
     }
